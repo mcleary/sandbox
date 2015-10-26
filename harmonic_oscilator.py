5b8bebd8e7350f0b928a7707696c6e844a25083c
mcleary <tluisrs@gmail.com>
2015-10-26 16:01:36 -0200

    Code to plot the simple harmonic oscilator vector field

---
+++ harmonic_oscilator.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def main():
+    print('Grid Resolution (10 is recomended): ', end='')
+    grid_resolution = int(input())
+    x = np.linspace(-100, 100, grid_resolution)
+    y = np.linspace(-100, 100, grid_resolution)
+    x, y = np.meshgrid(x, y)
+    
+    m = 1.0
+    b = 0.5
+    k = 0.5
+    
+    inv_m = 1.0 / m
+    
+    vx = y
+    vy = (-b*inv_m*y - k*inv_m*x)
+
+    # plot vector field
+    plt.quiver(x, y, vx, vy, pivot='middle', headwidth=4, headlength=6)
+    plt.xlabel('$x$')
+    plt.ylabel('$y$')
+    plt.axis('image')
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
