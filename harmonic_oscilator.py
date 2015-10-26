982ca137567101e291648e41a87934712b275763
mcleary <tluisrs@gmail.com>
2015-10-26 17:58:37 -0200

    Change in the grid resolution for the harminic oscilator vector field plot

--- harmonic_oscilator.py
+++ harmonic_oscilator.py
@@ -7,10 +7,10 @@ def main():
     
     inv_mass = 1.0 / mass
     
-    def dxdt(x,y): return y
-    def dydt(x,y): return (-damping_constant * inv_mass * y) - (spring_constant * inv_mass * x)
+    def dxdt(x, y): return y
+    def dydt(x, y): return (-damping_constant * inv_mass * y) - (spring_constant * inv_mass * x)
     
-    plot.plot_vector_field(dxdt, dydt, normalize=False)    
+    plot.plot_vector_field(dxdt, dydt, normalize=False, grid_resolution=20)    
     
 
 if __name__ == '__main__':
